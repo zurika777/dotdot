5552fa3fb0e95c7ca68a39042476fa3e27c9ff69
Admin <zurika777@gmail.com>
2021-03-24 21:28:17 +0400

    v 1.1 header and slider and about

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,13 @@
-console.log(1);
+$(function() {
+$("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+
+    var blockId = $(this).data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+    $("html, body").animate({
+        scrollTop: blockOffset
+    }, 500);
+
+});
+});
