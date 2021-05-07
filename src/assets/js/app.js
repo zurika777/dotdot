61daa4999b9b84f7d794df72af39eac2d2a554b1
Admin <zurika777@gmail.com>
2021-05-07 23:24:10 +0400

    v 1.1 header and slider and about and statistics and services and servoces and accordion and slider prev next and card and logos and works and clients and blogs and instagram and adaptathion and smooth scrol - active class

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,13 +1,47 @@
 $(function() {
-$("[data-scroll]").on("click", function(event) {
+
+    /*Smooth scrool*/
+
+    $("[data-scroll]").on("click", function(event) {
     event.preventDefault();
 
-    var blockId = $(this).data('scroll'),
+    var $this = $(this),
+        blockId = $(this).data('scroll'),
         blockOffset = $(blockId).offset().top;
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
 
     $("html, body").animate({
         scrollTop: blockOffset
     }, 500);
 
 });
+
+    /*header fixed*/
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+
+    });
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= introH ) {
+            header.addClass("header--fixed");
+        } else {
+            header.removeClass("header--fixed");
+        }
+    }
+
+
+
+
+
+
+
 });
